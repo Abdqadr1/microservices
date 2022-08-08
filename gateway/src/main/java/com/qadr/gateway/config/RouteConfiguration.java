d4cadd9865cb20f4869d1e7729131e53203d9d43
Abdqadr1 <abolarinwaquadri@gmail.com>
2022-08-09 07:03:17 +0800

    api gateway

---
+++ gateway/src/main/java/com/qadr/gateway/config/RouteConfiguration.java
@@ -0,0 +1,35 @@
+package com.qadr.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfiguration {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("path_route", r -> r.path("/get")
+                        .uri("http://httpbin.org"))
+                .route("customer", r -> r.path("/customer/**")
+                        .uri("lb://customer"))
+                .route("open-feign", r -> r.path("/c/**")
+                        .uri("lb://open-feign"))
+//                .route("rewrite_route", r -> r.host("*.rewrite.org")
+//                        .filters(f -> f.rewritePath("/foo/(?<segment>.*)", "/${segment}"))
+//                        .uri("http://httpbin.org"))
+//                .route("hystrix_route", r -> r.host("*.hystrix.org")
+//                        .filters(f -> f.hystrix(c -> c.setName("slowcmd")))
+//                        .uri("http://httpbin.org"))
+//                .route("hystrix_fallback_route", r -> r.host("*.hystrixfallback.org")
+//                        .filters(f -> f.hystrix(c -> c.setName("slowcmd").setFallbackUri("forward:/hystrixfallback")))
+//                        .uri("http://httpbin.org"))
+//                .route("limit_route", r -> r
+//                        .host("*.limited.org").and().path("/anything/**")
+//                        .filters(f -> f.requestRateLimiter(c -> c.setRateLimiter(redisRateLimiter())))
+//                        .uri("http://httpbin.org"))
+                .build();
+    }
+}
