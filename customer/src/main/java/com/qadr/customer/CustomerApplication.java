eee1631d38d31d6c1c0b6ca56fbff15af8448d9a
Abdqadr1 <abolarinwaquadri@gmail.com>
2022-08-22 20:47:19 +0800

    spring cloud gateway oauth2 security

--- customer/src/main/java/com/qadr/customer/CustomerApplication.java
+++ customer/src/main/java/com/qadr/customer/CustomerApplication.java
@@ -4,7 +4,6 @@ import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
